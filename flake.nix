1d85b730c718226b4b3fec267747a428fe733b2d
James Carragher <james@carragher.dev>
2025-08-15 14:02:14 +0100

    nixpkgs.config.allowUnfree

--- flake.nix
+++ flake.nix
@@ -17,7 +17,10 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       in
       {
         packages.neovim = pkgs.callPackage ./neovim.nix { };
